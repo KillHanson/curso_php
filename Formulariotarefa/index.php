48661e052f9744414260226179ee8498f9bdad3a
Kiseane <kiseane@gmail.com>
2022-02-11 23:47:09 -0300

    formulario tarefa

---
+++ Formulariotarefa/index.php
@@ -0,0 +1,43 @@
+<!--AQUI E HTML-->
+
+<form method="POST" action="exibir_dados.php">
+
+    ===FORMULARIO===
+
+    </br></br>Nome:
+    <input type="text" name="frm_name">
+
+    </br></br>Idade:
+    <input type="number" name="frm_idade">
+
+    </br></br>Sexo:
+    <select name="frm_sexo">
+        <option value="M">Masculino</option>
+        <option value="F">Feminino</option>
+    </select>
+
+    </br></br><button type="submit">Enviar</button>
+
+    </br></br>Senha:
+    <input type= "text" name="frm_senha">
+        <input
+
+    </br></br><button type="submit">Enviar</button>
+
+    </br></br>CPF:
+    <input type="text" name="frm_CPF">
+
+    </br></br>LOGIN:
+    <input type="text" name="frm_login">
+
+    </br></br><button type="submit">Enviar</button>
+
+
+
+</form>
+
+<?php
+    //AQUI E PHP
+
+    
+?>
